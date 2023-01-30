cdbd12ad5aad11126d28cc3934db75697bb6dea7
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2023-01-30 01:02:02 +0100

    review model done

---
+++ models/Review.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+ 
+const reviewSchema = new Schema(
+  // Add whichever fields you need for your app
+  {
+    stars: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+      },
+      comment: {
+        type: String
+      },
+      username: {
+        type: String
+      },
+      show: {
+        type: Schema.Types.ObjectId, // Solo uno
+        ref: 'Course'
+      }
+    })
+ 
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
