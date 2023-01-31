4d41ed18629e3fd4a23e97f88481cc890ca24289
MoniCamargo37 <moni28ac@gmail.com>
2023-01-31 10:20:49 +0100

    modified some fields in user model

--- models/User.js
+++ models/User.js
@@ -1,36 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
- 
+
 const userSchema = new Schema(
   // Add whichever fields you need for your app
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, "Username is required."],
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      required: true
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     hashedPassword: {
       type: String,
-      required: [true, 'Password is required.'],
+      required: [true, "Password is required."],
     },
     googleID: {
-      type: String,
-  },
-    facebookID: {
+      //we are going to use it when we create a social login account
       type: String,
     },
-    timestamps: {}
+    facebookID: {
+      //we are going to use it when we create a social login account
+      type: String,
+    },
+    paymentCard: {
+      //we are going to use this field for purchase simulation(fake)
+      type: Number,
+      required: [true, "Payment card is required."],
+    },
+    expiryDate: {
+      type: Number,
+      required: [true, "expiry date is required."],
+    },
+    cvv: {
+      type: Number,
+      required: [true, "CVV is required."],
+    },
+    nameOnCard: {
+      type: String,
+      required: [true, "card name is required."],
+    },
+    country: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
   }
 );
- 
-const User = mongoose.model('User', userSchema);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
