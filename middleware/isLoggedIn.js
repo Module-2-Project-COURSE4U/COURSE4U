f8c066c7350227d8383cb60f9764ac6fa459215c
MoniCamargo37 <moni28ac@gmail.com>
2023-02-07 20:35:52 +0100

    delete route

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,7 +1,7 @@
-module.exports = isLoggedIn = (req, res, next) => {
-  if (!req.session.currentUser) {
-    res.redirect("/auth/login");
-  } else {
-    next();
-  }
-};
+// module.exports = isLoggedIn = (req, res, next) => {
+//   if (!req.session.currentUser) {
+//     res.redirect("/auth/login");
+//   } else {
+//     next();
+//   }
+// };
