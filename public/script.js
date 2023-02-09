da00f666c97fc2b4169a904f54f25e8c199a4d37
MoniCamargo37 <moni28ac@gmail.com>
2023-02-09 01:22:13 +0100

    navbar

--- public/script.js
+++ public/script.js
@@ -13,3 +13,14 @@ cardContainer.addEventListener('click', event => {
   }
 });
 
+/* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
+addEventListener('DOMContentLoaded', () => {
+const btn_menu = document.querySelector('.btn_menu')
+if (btn_menu) {
+  btn_menu.addEventListener('click',() =>{
+    const menu_items   = document.querySelector('.menu_items')
+      menu_items.classList.toggle('show')
+  });
+}
+});
+
